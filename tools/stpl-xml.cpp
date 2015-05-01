410945c43bd28c52c6cc1f7918107ffb42a22452
Monfee <eric.tang2005@gmail.com>
2015-05-01 17:47:18 +1000

    need debug

--- tools/stpl-xml.cpp
+++ tools/stpl-xml.cpp
@@ -31,11 +31,13 @@
 #endif
 
 using namespace FILESYSTEM;
+using namespace std;
 
 void usage(const char *program) {
 	fprintf(stderr, "%s of STPL (Simple Text Processing Library) - a simple XML value extraction tool (version: %s)", program, VERSION);
 	fprintf(stderr, "\n\n");
-	fprintf(stderr, "usage: %s xpath[node:attr] /a/path/to/xml/file");
+	fprintf(stderr, "usage: %s xpath /a/path/to/xml/file"); // [node:attr]
+	fprintf(stderr, "          xpath - node[/childnode]:attr");
 	exit(-1);
 }
 
@@ -48,6 +50,50 @@ int main(int argc, char* argv[])
 
 	if (!File<>::exists(file)) {
 		fprintf(stderr, "no such file: %s", file);
+	}
+
+	File<string, char *> fs(argv[1]);
+	str = string(fs.begin(), fs.end());
+
+	typedef XML::XParser<string, string::const_iterator> 		xml_parser;
+	typedef xml_parser::document_type::entity_type	  			node_type;
+	typedef xml_parser::document_type::entity_iterator 		entity_iterator;
+	typedef xml_parser::document_type::element_type			element_type;
+	xml_parser parser(str.begin(), str.end());
+	parser.parse();
+
+	xml_parser::document_type &doc = parser.doc();
+
+	const char *xpath = argv[1];
+	const char *pos = xpath;
+	std::string first_tag;
+	while (*pos != '\0' && *pos != '/' && *pos != '[')
+		first_tag.push_back(*pos++);
+
+	int index = 0;
+	if (*pos == '[')
+		index = atoi(++pos);
+
+	int count = 0;
+	// allow non-stard xml file with no single document root
+	for (it = doc.iter_begin(); it != doc.iter_end(); ++it) {
+		node_type *node = static_cast<node_type*>((*it));
+
+		if (node->is_element()) {
+			element_type *elem = static_cast<element_type *>(node);
+			if (elem->name() == first_tag && count == index) {
+				if (*pos == '/') {
+					element_type *node = elem->get_descendent_node_by_xpath(++pos);
+				}
+				else if (*pos == ':')
+					cout << elem->get_attribute(++pos);
+				else
+					cout << elem->text();
+
+				break;
+			}
+		}
+		++count;
 	}
 	return 0;
 }
